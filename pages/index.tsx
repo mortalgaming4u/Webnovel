e3a9679aacf7ffd2101459222ed24532750a19a7
mortalgaming4u <mortalgaming4u@gmail.com>
2025-09-03 14:35:57 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Reader from "../components/Reader";
 
 export default function Home() {
+  const [chapterText, setChapterText] = useState<string>("");
+  const [progress, setProgress] = useState<number>(0);
+
   return (
-    <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Reader />
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <div className="w-72 border-r border-gray-200">
+        <Sidebar setChapterText={setChapterText} setProgress={setProgress} />
+      </div>
+
+      {/* Reader */}
+      <div className="flex-1 p-4 overflow-y-auto">
+        <Reader
+          chapterText={chapterText}
+          progress={progress}
+          setChapterText={setChapterText}
+          setProgress={setProgress}
+        />
       </div>
     </div>
   );
