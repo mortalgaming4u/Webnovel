73e8ac04142629a67fb0a7784e7565fe290acecc
mortalgaming4u <mortalgaming4u@gmail.com>
2025-09-03 14:31:55 +0530

    Update sites.ts

--- utils/sites.ts
+++ utils/sites.ts
@@ -1,19 +1,29 @@
-// Supported site URL patterns
-export const sites = [
-  "https://fanqienovel.com/page/",
-  "https://www.qimao.com/shuku/",
-  "https://www.uuread.tw/",
-  "https://www.69shuba.com/book/",
-  "https://twkan.com/book/",
-  "https://www.twbook.cc/",
-  "https://www.piaotia.com/bookinfo/",
-  "https://www.trxs.cc/tongren/",
-  "https://tongrenshe.cc/tongren/",
-  "https://uukanshu.cc/book/",
-  "https://m.bixiange.me/book/",
-  "https://www.ffxs8.com/book/",
-  "https://www.biquge.tw/book/",
-  "https://101kanshu.com/book/",
-  "https://www.drxsw.com/book/",
-  "https://m.ddxs.com/"
+/**
+ * site rules mapping - add more patterns/selectors as you discover per-site structure.
+ * The scraper API uses heuristics; this file can be referenced by future server-side improvements.
+ */
+export type SiteRule = {
+  hostPattern: RegExp;
+  indexSelectors?: string[]; // selectors to find chapter links on index pages
+  chapterSelectors?: string[]; // selectors to extract chapter content
+};
+
+const rules: SiteRule[] = [
+  {
+    hostPattern: /twkan\.com/i,
+    indexSelectors: [".mulu", ".chapter-list", ".list", ".chapter-list a"],
+    chapterSelectors: ["#BookText", "#content", ".readarea", ".chapter"]
+  },
+  {
+    hostPattern: /ixdzs\.tw/i,
+    indexSelectors: [".catalog", ".chapters", ".chapter-list"],
+    chapterSelectors: ["#chapterContent", ".main-text", ".read-content"]
+  },
+  {
+    hostPattern: /biquge\.tw/i,
+    indexSelectors: [".booklist", ".chapter-list"],
+    chapterSelectors: ["#content", ".content"]
+  }
 ];
+
+export default rules;
